e5d4b53f7534d79fa146331bc4b95c0d07f1e795
hibikiprime0098-dotcom <hibikiprime0098@gmail.com>
2026-09-21 04:34:19 +0900

    Deploy build 1006 contrast fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hangeorum-v1005-unified';
+const CACHE='hangeorum-v1006-unified';
 const CORE=[
   "./",
   "./index.html",
@@ -36,6 +36,7 @@ const CORE=[
   "./v953-promotion-same-format.js",
   "./v955-current-ui.js",
   "./v1000-latest-share.js",
+  "./v1001-contrast-hotfix.js",
   "./share/",
   "./share/index.html"
 ];
