5f5389e44735d092563fb4c6023ef19d5caa5d9d
hibikiprime0098-dotcom <hibikiprime0098@gmail.com>
2026-09-21 00:58:56 +0900

    Set current app build marker to 1003

--- v1000-latest-share.js
+++ v1000-latest-share.js
@@ -1,7 +1,7 @@
 (()=>{
 if(window.__HANGEORUM_V1000__)return;window.__HANGEORUM_V1000__=1;
 window.HANGEORUM_VERSION='10.0';
-window.HANGEORUM_BUILD='1002';
+window.HANGEORUM_BUILD='1003';
 document.title='한걸음 | 韓国語学習 v10.0';
 
 const params=new URLSearchParams(location.search);
